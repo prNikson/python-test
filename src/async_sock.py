165715314d5285f02e110d362c5a1fe50d82a502
Sergey <sergionik03@gmail.com>
2025-03-30 11:22:27 +0300

    edit classmethod

--- src/async_sock.py
+++ src/async_sock.py
@@ -4,10 +4,8 @@ import json
 import toml
 from http_handlers import *
 import logging
+from exception import ParseFileError
 
-
-# HTTPres = HTTPResponse()
-# HTTPreq = HTTPRequest()
 filename = 'spec.toml'
 logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="a",
                     format="%(asctime)s %(levelname)s\n%(message)s\n")
@@ -20,20 +18,20 @@ class Request:
         self.message = mes
     
     async def _prepare_request(self) -> int:
-        body = json.dumps({
+        self.body = {
             "sender": self.send,
             "recipient": self.recip,
             "message": self.message
-        })
+        }
+        self.body = json.dumps(self.body)
         if await self._get_info_from_toml():
 
             credentials = base64.b64encode(f"{self.login}:{self.passwd}".encode()).decode()
-            self.request = f"POST /{self.point} HTTP/1.1\r\n"
-            host = f"Host: {self.address}:{self.port}/{self.point}\r\n"
-            content_type = "Content-Type: application/json\r\n"
-            content_length = f"Content-Length: {str(len(body))}\r\n"
-            auth = f"Authorization: Basic {credentials}\r\n\r\n"
-            self.request += "".join([host, content_type, content_length, auth, body])
+            self.headers = {
+                "Content-Type": "application/json\r\n",
+                "Content-Length": f"{str(len(self.body))}\r\n",
+                "Authorization": f"Basic {credentials}\r\n\r\n"
+            }
             return 1
         return 0
 
@@ -41,26 +39,32 @@ class Request:
         try:
             with open(filename, "r") as file:
                 data = toml.load(file)
+            try:
                 self.login, self.passwd = data['user']['login'], data['user']['password']
                 server = data['server']
                 self.address = server['address']
                 self.port = server['port']
-                self.point = server['point']
-            return 1
+                self.point = self.address.split('/')[-1]
+            except ParseFileError:
+                logging.error("Error: The field(s) don`t parse from file")
+                return 0
         except FileNotFoundError:
             logging.error(f"Error: File {filename} not found")
             return 0
+        return 1
 
     async def post_request(self) -> None:
         if await self._prepare_request():
-            reader, writer = await asyncio.open_connection(self.address, self.port)
-            writer.write(self.request.encode())
+            address = self.address.split('/')[0]
+            reader, writer = await asyncio.open_connection(address, self.port)
+            HTTPreq = HTTPRequest(self.body, self.address, self.headers)
+            writer.write(HTTPreq.to_bytes())
             await writer.drain()
             response = await reader.read(4096)
-            # print(response)
-            # HTTPResponse.from_bytes(response)
+            HTTPresp = HTTPResponse.from_bytes(response)
+            print(HTTPresp.code, HTTPresp.body)
             writer.close()
 
-            # logging.info(f"Host:{self.address}:{self.port}/{self.point}\nsend:{self.send}\n" +\
-            #     f"recipient:{self.recip}\nmessage:{self.message}\n" +
-            #     HTTPResponse.code + "\n" + json.dumps(HTTPResponse.body))
+            logging.info(f"Host:{self.address} Port: {self.port}\nsend:{self.send}\n" +\
+                f"recipient:{self.recip}\nmessage:{self.message}\n" +
+                HTTPresp.code + "\n" + json.dumps(HTTPresp.body))
--- src/http_handlers.py
+++ src/http_handlers.py
@@ -5,21 +5,17 @@ from typing import Self
 
 class HTTPRequest:
 
-    def __init__(self, body: dict[str, str], address: str, headers: dict[str, str]):
+    def __init__(self, body: str, address: str, headers: dict[str, str]):
         self.body = body
         self.headers = headers
         self.address = address
 
     def to_bytes(self) -> bytes:
         point = self.address.split('/')[-1]
-        self.body = json.dumps(self.body)
-        self.headers = json.dumps(self.headers)
         request = f"POST /{point} HTTP/1.1\r\n"
         host = f"Host: {self.address}\r\n"
-        # content_type = "Content-Type: application/json\r\n"
-        # content_length = f"Content-Length: {str(len(self.body))}\r\n"
-        # auth = f"Authorization: Basic {self.credentials}\r\n\r\n"
-        request += "".join([host, self.headers, self.body])
+        gen = [f"{k}: {v}" for k, v in self.headers.items()]
+        request += "".join([host, "".join(gen), self.body])
         return request.encode('utf-8')
     
     @classmethod
@@ -36,22 +32,16 @@ class HTTPResponse:
     
     def __init__(self, code, body):
         self.code = code
-        # self.headers = headers
         self.body = body
 
     def to_bytes(self) -> bytes:
         code = f"HTTP/1.1 {self.code}"
-        return (self.code + '\r\n\r\n' + self.body).encode()
+        return (code + '\r\n\r\n' + self.body).encode()
 
     @classmethod
     def from_bytes(cls, resp: bytes) -> Self:
         response = (resp.decode()).split('\r\n')
-        # print(response)
         code = (re.split(r'HTTP/1.1', response[0])[-1]).strip()
         body = json.loads(response[-1])
-        # response = [i.split(':') for i in response[5:7]]
-        # headers = {key.strip(): value.strip() for key, value in response}
         return cls(code, body)
-
-a = HTTPResponse.from_bytes(b'HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Headers: *\r\nAccess-Control-Allow-Credentials: true\r\nAccess-Control-Expose-Headers: *\r\nContent-type: application/json\r\nContent-Length: 42\r\nDate: Sat, 29 Mar 2025 11:01:42 GMT\r\nConnection: keep-alive\r\nKeep-Alive: timeout=5\r\n\r\n{"status":"success","message_id":"123456"}')
-print(vars(a))
+# a = HTTPResponse.from_bytes(b'HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Headers: *\r\nAccess-Control-Allow-Credentials: true\r\nAccess-Control-Expose-Headers: *\r\nContent-type: application/json\r\nContent-Length: 42\r\nDate: Sat, 29 Mar 2025 11:01:42 GMT\r\nConnection: keep-alive\r\nKeep-Alive: timeout=5\r\n\r\n{"status":"success","message_id":"123456"}')
